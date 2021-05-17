68fcea428b99234211758e4fa540884415f32fc1
Kubsti <64650408+Kubsti@users.noreply.github.com>
2021-05-17 15:44:34 +0200

    Found simpler way to show XSS insecurities

--- env/app.py
+++ env/app.py
@@ -3,10 +3,6 @@ from databasemanagement import getposts
 from jinja2 import Environment, select_autoescape
 
 app = Flask(__name__)
-#should never be done and is only there to show that if autoescape is distabled then the application is open for XSS attacks
-env =Environment(autoescape=select_autoescape(
-    disabled_extensions=('html')
-))
 @app.route("/")
 def home():
     qrows = getposts()
