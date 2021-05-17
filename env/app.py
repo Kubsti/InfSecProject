c5850944d3a507de5ab0285a667d9372310c3306
Kubsti <64650408+Kubsti@users.noreply.github.com>
2021-05-17 14:15:36 +0200

    Jinja functionality implemented

--- env/app.py
+++ env/app.py
@@ -1,7 +1,13 @@
 from flask import Flask, render_template
+from databasemanagement import getposts
+from jinja2 import Environment, select_autoescape
 
 app = Flask(__name__)
-
+#should never be done and is only there to show that if autoescape is distabled then the application is open for XSS attacks
+env =Environment(autoescape=select_autoescape(
+    disabled_extensions=('html')
+))
 @app.route("/")
 def home():
-    return render_template('index.html', status=True)
+    qrows = getposts()
+    return render_template('index.html', rows = qrows, status=True)
--- env/databasemanagement.py
+++ env/databasemanagement.py
@@ -1,14 +1,14 @@
 import psycopg2
 
-conn = psycopg2.connect(database="testforum",user="postgres",password="Kubsti4146",host="127.0.0.1",port="5432")
-cur = conn.cursor()
+def getposts():
+    try:
+        conn = psycopg2.connect(database="testforum",user="postgres",password="Kubsti4146",host="127.0.0.1",port="5432")
+        cur = conn.cursor()
+        cur.execute("SELECT posttitel, username from posts,hasposts,forumuser WHERE forumuser.randuserid = hasposts.userid AND hasposts.postid = posts.postid")
+        rows = cur.fetchall()
+        conn.close()
+    except psycopg2.OperationalError as e:
+        print('Unable to connet!\n{0}').format(e)
+    
+    return rows    
 
-cur.execute("SELECT postid, postcontent, posttitel from posts")
-rows = cur.fetchall()
-
-for row in rows:
-    print ("POSTID = ", row[0])
-    print ("POSTCONTENT = ", row[1])
-    print ("POSTTITEL = ", row[2])
-
-conn.close()
