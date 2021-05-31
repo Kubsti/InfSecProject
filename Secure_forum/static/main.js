49f9488668eaf753d69d1db759ce4f8d90d96bb5
Kubsti <64650408+Kubsti@users.noreply.github.com>
2021-05-31 14:46:05 +0200

    Refractoring

--- Secure_forum/static/main.js
+++ Secure_forum/static/main.js
@@ -46,11 +46,8 @@ function createPost(){
 
 function checkifloggedin(){
     var pathname = window.location.pathname;
-    if(current_user.is_authenticated){
       if(pathname == '/register' || pathname == '/login'){
       window.location.href = "http://127.0.0.1:5000/home";
-      }
-    }
-    
+    }  
 }
 
