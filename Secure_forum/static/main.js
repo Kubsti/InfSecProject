d11533766227dd455a33c0e3d4c231905d1c0779
Kubsti <64650408+Kubsti@users.noreply.github.com>
2021-06-01 14:06:03 +0200

    Refractoring

--- Secure_forum/static/main.js
+++ Secure_forum/static/main.js
@@ -43,11 +43,3 @@ function createPost(){
         });       
 }
 
-
-function checkifloggedin(){
-    var pathname = window.location.pathname;
-      if(pathname == '/register' || pathname == '/login'){
-      window.location.href = "http://127.0.0.1:5000/home";
-    }  
-}
-
